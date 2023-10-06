9d0b74b8394c4f7e5313242a0d5eb7ed3ee15834
Arjun chand <chandarjun10@gmail.com>
2023-10-06 18:39:05 +0530

    added getters and setters

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BankAccount bankAccount = new BankAccount("John", 5000, 1000);
+        //BankAccount bankAccount = new BankAccount("John", 5000, 1000);
 
         // Generate an account number with 6 digits and a sum of 24
-        bankAccount.generateAccountNumber(6, 24);
+        //bankAccount.generateAccountNumber(6, 24);
     }
 }
